218106f298f8918661588405c2b25eda35ab899f
Miguel Rodriguez <miguelrodrii@outlook.com>
2021-05-28 22:48:28 -0500

    env

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,11 +1,11 @@
 import pgPromise from 'pg-promise';
 import {env} from "../config/env.js"
 
-const dbUser = process.env.DB_USER || env.parsed.DB_USER;
-const dbPass = process.env.DB_PASS || env.parsed.DB_PASS;
-const dbHost = process.env.DB_HOST || env.parsed.DB_HOST;
-const dbPort = process.env.DB_PORT || env.parsed.DB_PORT;
-const dbName = process.env.DB_NAME || env.parsed.DB_NAME;
+const dbUser = env.parsed.DB_USER;
+const dbPass = env.parsed.DB_PASS;
+const dbHost = env.parsed.DB_HOST;
+const dbPort = env.parsed.DB_PORT;
+const dbName = env.parsed.DB_NAME;
 
 // Create Database Connection
 const pgp = pgPromise({});
