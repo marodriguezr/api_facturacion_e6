99cbae57baf1bf7901d11e9e2526d0e00782a4f9
Miguel Rodriguez <miguelrodrii@outlook.com>
2021-05-28 23:04:04 -0500

    env

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,12 +1,13 @@
 import pgPromise from 'pg-promise';
 import {env} from "../config/env.js"
 
-// const dbUser = env.parsed.DB_USER;
-// const dbPass = env.parsed.DB_PASS;
-// const dbHost = env.parsed.DB_HOST;
-// const dbPort = env.parsed.DB_PORT;
-// const dbName = env.parsed.DB_NAME;
+const dbUser = process.env.DB_USER || env.parsed.DB_USER;
+const dbPass = process.env.DB_PASS || env.parsed.DB_PASS;
+const dbHost = process.env.DB_HOST || env.parsed.DB_HOST;
+const dbPort = process.env.DB_PORT || env.parsed.DB_PORT;
+const dbName = process.env.DB_NAME || env.parsed.DB_NAME;
+const ssl = (env.parsed.SSL || "true") == "false" ? "" : "?ssl=true";
 
 // Create Database Connection
 const pgp = pgPromise({});
-export const db = pgp(process.env.DATABASE_URL || "");
+export const db = pgp(`postgres://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}${ssl}`);
