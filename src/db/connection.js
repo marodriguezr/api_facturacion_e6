e0f7a8573c4db66fb438c8b5581cb6abb4100b89
Miguel Rodriguez <miguelrodrii@outlook.com>
2021-05-28 22:29:37 -0500

    .env

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,12 @@
 import pgPromise from 'pg-promise';
 import {env} from "../config/env.js"
 
+const dbUser = process.env.DB_USER || env.parsed.DB_USER;
+const dbPass = process.env.DB_PASS || env.parsed.DB_PASS;
+const dbHost = process.env.DB_HOST || env.parsed.DB_HOST;
+const dbPort = process.env.DB_PORT || env.parsed.DB_PORT;
+const dbName = process.env.DB_NAME || env.parsed.DB_NAME;
+
 // Create Database Connection
 const pgp = pgPromise({});
-export const db = pgp(`postgres://${env.parsed.DB_USER}:${env.parsed.DB_PASS}@${env.parsed.DB_HOST}:${env.parsed.DB_PORT}/${env.parsed.DB_NAME}`);
+export const db = pgp(`postgres://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import {routes} from "./routes/index.js";
 import { env } from "./config/env.js"
 
-const PORT = env.parsed.PORT || 5000
+const PORT = process.env.PORT || env.parsed.PORT
 
 const app = express();
 
