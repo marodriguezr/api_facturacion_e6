5ffce73d8c77fc7bdc0b0fc7cd0bf6a98dd67daf
Diego Castillo <ddcastillog@utn.edu.ec>
2021-06-01 12:14:05 -0500

    billsHeaders crud

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,16 +1,12 @@
 import Sequelize from 'sequelize'
 
-const dbUrl = process.env.DATABASE_URL === undefined ? "postgres://postgres:admin@localhost:5432/api_facturacion_a6" : process.env.DATABASE_URL;
+const dbUrl = process.env.DATABASE_URL === undefined ? "postgres://postgres:1234@localhost:5432/api_facturacion_a6" : process.env.DATABASE_URL;
 
 export const sequelize = new Sequelize(
     dbUrl,
     {
         dialect: 'postgres',
-        "dialectOptions": {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false // <<<<<<< YOU NEED THIS
-            }
-        }
+        protocol: 'postgres',
+        dialectOptions: {}        
     }
 )
