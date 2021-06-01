9c4ca5bb092229ae13ef1e84f8e360f24778a95a
derickds <60703366+derickds@users.noreply.github.com>
2021-06-01 16:14:05 -0500

    clientsTypesController

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,16 +1,12 @@
 import Sequelize from 'sequelize'
 
-const dbUrl = process.env.DATABASE_URL === undefined ? "postgres://postgres:admin@localhost:5432/api_facturacion_a6" : process.env.DATABASE_URL;
+const dbUrl = process.env.DATABASE_URL === undefined ? "postgres://postgres:postgres@localhost:5432/api_facturacion_a6" : process.env.DATABASE_URL;
 
 export const sequelize = new Sequelize(
     dbUrl,
     {
         dialect: 'postgres',
         "dialectOptions": {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false // <<<<<<< YOU NEED THIS
-            }
         }
     }
 )
