92afff357b659bfa91de24de5e2a4b92d98e0749
Diego Castillo <ddcastillog@utn.edu.ec>
2021-06-02 08:24:42 -0500

    ss

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,5 @@
 import Sequelize from 'sequelize'
 
-const dbUrl = process.env.DATABASE_URL === undefined ? "postgres://postgres:admin@localhost:5432/api_facturacion_a6" : process.env.DATABASE_URL;
 
 export const sequelize = new Sequelize(
     dbUrl,
