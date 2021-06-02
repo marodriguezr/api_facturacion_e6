7dc9c8c57d57394c9cc4afdecafdcbe8dae99241
Diego Castillo <ddcastillog@utn.edu.ec>
2021-06-02 08:26:02 -0500

    coneccion

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize'
+const dbUrl = process.env.DATABASE_URL === undefined ? "postgres://mocewaneegbxpq:3cc81a1184968f522e4313699e3478ae836edf87c9acd9766b743815423ba808@ec2-18-215-111-67.compute-1.amazonaws.com:5432/decuq28hhilr9p" : process.env.DATABASE_URL;
 
 
 export const sequelize = new Sequelize(
